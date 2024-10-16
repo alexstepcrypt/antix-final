2c5a405ad271368e35936cdef1148b67ccf37e47
tragedy <feqt26@gmail.com>
2024-10-16 17:53:59 +0500

    feat: added mobile layout for antix-token

--- src/sections/AntixToken/AntixToken.tsx
+++ src/sections/AntixToken/AntixToken.tsx
@@ -1,14 +1,43 @@
+import Image from 'next/image';
+
+import coin from '@/public/svg/token/coin.svg';
 import glow from '@/public/images/token-glow.png';
+import { FadeInNew } from '@/components/FadeInNew/FadeInNew';
+import { TokenCard } from './components/TokenCard/TokenCard';
 import { TokenHead } from './components/TokenHead/TokenHead';
 import { TokenBody } from './components/TokenBody/TokenBody';
+import { mobileList } from './mocdata';
 import s from './AntixToken.module.scss';
 
 const AntixToken = () => {
    return (
-      <div style={{ backgroundImage: `url(${glow.src})` }} className={s.wrapper} id='Token'>
+      <div
+         style={{ backgroundImage: `url(${glow.src})` }}
+         className={s.wrapper}
+         id='ANTIXTokens'
+      >
          <div id="ANTIXTokens">
             <TokenHead />
             <TokenBody />
+
+            <div className={s.mobileToken}>
+               <FadeInNew direction='up'>
+                  <Image
+                     src={coin.src}
+                     alt="coin"
+                     width={725}
+                     height={510}
+                     loading="lazy"
+                     className={s.coin}
+                  />
+               </FadeInNew>
+            </div>
+
+            <div className={s.mobileList}>
+               {mobileList.map((item, i) => (
+                  <TokenCard key={i} {...item} />
+               ))}
+            </div>
          </div>
       </div>
    );
--- src/sections/AntixToken/components/TokenBody/TokenBody.tsx
+++ src/sections/AntixToken/components/TokenBody/TokenBody.tsx
@@ -1,55 +1,12 @@
+'use client';
+
 import Image from 'next/image';
 
 import coin from '@/public/svg/token/coin.svg';
 import bg from '@/public/svg/token/coin-bg.svg';
-import hummer from '@/public/svg/token/hummer.svg';
-import shop from '@/public/svg/token/shop.svg';
-import handshake from '@/public/svg/token/handshake.svg';
-import lines from '@/public/svg/token/lines.svg';
-import stars from '@/public/svg/token/stars.svg';
-import subscription from '@/public/svg/token/subscription.svg';
 import { TokenCard } from '../TokenCard/TokenCard';
 import s from './TokenBody.module.scss';
-import { TweenLite } from 'gsap'
-
-const leftList = [
-   {
-      title: 'NFT Auction & Promos',
-      description: 'Lend or sell avatars and assets. Promote to earn more.',
-      icon: hummer.src,
-   },
-   {
-      title: 'Asset Tuning',
-      description: 'Upgrade your digital assets — keep them trendy and fresh.',
-      icon: stars.src,
-   },
-];
-
-const midList = [
-   {
-      title: 'Subscriptions',
-      description: 'Unlock premium tools for digital creation with ANTIX.',
-      icon: subscription.src,
-   },
-   {
-      title: 'Marketplace',
-      description: 'Trade and customize your digital assets in the marketplace.',
-      icon: shop.src,
-   },
-];
-
-const rightList = [
-   {
-      title: 'Governance',
-      description: 'Vote on creators and rewards. Shape the future with ANTIX.',
-      icon: handshake.src,
-   },
-   {
-      title: 'Staking',
-      description: 'Stake your tokens and boost your rewards. Higher stake, higher earnings.',
-      icon: lines.src,
-   },
-];
+import { leftList, midList, rightList } from '../../mocdata'
 
 export const TokenBody = () => {
    return (
@@ -57,13 +14,19 @@ export const TokenBody = () => {
          style={{ backgroundImage: `url(${bg.src})` }}
          className={s.container}>
          <div className={s.left}>
-            {leftList.map((item, i) => <TokenCard key={i} {...item} />)}
+            {leftList.map((item, i) => (
+               <TokenCard key={i} {...item} />
+            ))}
          </div>
          <div className={s.middle}>
-            {midList.map((item, i) => <TokenCard key={i} {...item} />)}
+            {midList.map((item, i) => (
+               <TokenCard key={i} {...item} />
+            ))}
          </div>
          <div className={s.right}>
-            {rightList.map((item, i) => <TokenCard key={i} {...item} />)}
+            {rightList.map((item, i) => (
+               <TokenCard key={i} {...item} />
+            ))}
          </div>
 
          <Image
