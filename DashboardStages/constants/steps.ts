1f19d51c8e0f72c2297635ac17563445e55417a5
tragedy51 <feqt26@gmail.com>
2024-11-19 19:55:10 +0500

    feat: changes in deposit steps and changes in deposit form added

--- DashboardStages/constants/steps.ts
+++ DashboardStages/constants/steps.ts
@@ -1,7 +1,7 @@
 export const stage1Steps = [
-   { step: 1, title: 'Make a deposit', isCurrent: true },
-   { step: 2, title: 'Wait for Stage 1 to start', isCurrent: false },
-   { step: 3, title: 'Buy tokens', isCurrent: false }
+   { step: 1, title: 'Confirmation', isCurrent: true },
+   { step: 2, title: 'Transaction (Deposits)', isCurrent: false },
+   { step: 3, title: 'Success', isCurrent: false }
 ];
 
 export const stage2Steps = [
