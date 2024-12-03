63fa0253343d0dc96753ce8fb18ce2519f80fbb3
tragedyfiftyone <148686852+tragedy51@users.noreply.github.com>
2024-12-03 18:02:58 +0500

    feat: updates in stage widget added

--- components/StageWidget/StageWidget.tsx
+++ components/StageWidget/StageWidget.tsx
@@ -5,7 +5,7 @@ import s from './StageWidget.module.scss';
 
 
 export const StageWidget = () => {
-   const targetDate = new Date('2024-12-03T17:00:00Z');
+   const targetDate = new Date('2024-12-17T17:00:00Z');
    const [timeLeft, setTimeLeft] = useState({
       days: 0,
       hours: 0,
@@ -95,7 +95,7 @@ export const StageWidget = () => {
             </div>
 
             <div className={s.timer}>
-               <h2>Stage 2 starts in</h2>
+               <h2>Stage 2 ends in</h2>
                <div className={s.countdown}>
                   <div>
                      <span>{timeLeft.days.toString().padStart(2, '0')}</span>
