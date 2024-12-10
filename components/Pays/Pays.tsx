83783785b599f3e0d47627e6b5c955ca6840eee5
tragedy51 <feqt26@gmail.com>
2024-12-10 22:10:28 +0500

    feat: changes in landing and dashboard added

--- components/Pays/Pays.tsx
+++ components/Pays/Pays.tsx
@@ -1,122 +1,32 @@
 import React from 'react';
 import styles from './Pays.module.scss';
 import Image from 'next/image';
+import usdt from '/public/svg/tether-icon.svg';
+import usdc from '/public/svg/usdc-icon.svg';
+import bnb from '/public/svg/bnb-icon.svg';
+import eth from '/public/svg/ether-icon.svg';
+import degen from '/public/svg/degen-coin.svg';
+import wethera from '/public/svg/wethera-coin.svg';
+import cbbtc from '/public/svg/cbbtc-coin.svg';
+import mantra from '/public/svg/mantra-coin.svg';
+
+const paysIcons = [usdt, usdc, bnb, eth, degen, wethera, cbbtc, mantra];
 
 const Pays = () => {
   return (
     <div className={styles.pays}>
       <h3 className={styles.paysTitle}>Pay with</h3>
 
-      <div className={styles.paysCards}>
-        <div className={styles.paysCard}>
+      <div className={styles.paysIcons}>
+        {paysIcons.map((icon, i) => (
           <Image
-            src={'/svg/tether-icon.svg'}
-            alt="USDT"
-            width={35.45}
-            height={35.45}
+            key={i}
+            src={icon}
+            alt="icon"
+            width={24}
+            height={24}
           />
-          <div>
-            <span>USDT</span>
-
-            <div className={styles.networks}>
-              <Image
-                className={styles.network}
-                src={'/svg/ether-icon.svg'}
-                alt="eth"
-                width={12.5}
-                height={12.5}
-              />
-              <Image
-                className={styles.network}
-                src={'/svg/network-icon.svg'}
-                alt="bnb"
-                width={12.5}
-                height={12.5}
-              />
-            </div>
-          </div>
-        </div>
-        <div className={styles.paysCard}>
-          <Image
-            src={'/svg/usdc-icon.svg'}
-            alt="USDC"
-            width={35.45}
-            height={35.45}
-          />
-          <div>
-            <span>USDC</span>
-
-            <div className={styles.networks}>
-              <Image
-                className={styles.network}
-                src={'/svg/ether-icon.svg'}
-                alt="eth"
-                width={12.5}
-                height={12.5}
-              />
-              <Image
-                className={styles.network}
-                src={'/svg/network-icon.svg'}
-                alt="bnb"
-                width={12.5}
-                height={12.5}
-              />
-            </div>
-          </div>
-        </div>
-        <div className={styles.paysCard}>
-          <Image
-            src={'/svg/bnb-icon.svg'}
-            alt="BNB"
-            width={35.45}
-            height={35.45}
-          />
-          <div>
-            <span>BNB</span>
-
-            <div className={styles.networks}>
-              <Image
-                className={styles.network}
-                src={'/svg/network-icon.svg'}
-                alt="bnb"
-                width={14.5}
-                height={14.5}
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-      <div
-        style={{
-          justifyContent: 'center',
-          marginTop: 10,
-          gap: 7,
-        }}
-        className={styles.paysCards}
-      >
-        <div className={styles.paysCard1}>
-          <Image
-            src={'/svg/ether-icon.svg'}
-            alt="USDC"
-            width={17}
-            height={17}
-          />
-          <span>ETH</span>
-        </div>
-        <div className={styles.paysCard1}>
-          <Image
-            src={'/dashboard/svg/visa-logo.svg'}
-            alt="visa"
-            width={30.8}
-            height={13}
-          />
-          <Image
-            src={'/dashboard/svg/mastercard-logo.svg'}
-            alt="mastercard"
-            width={24.77}
-            height={19}
-          />
-        </div>
+        ))}
       </div>
     </div>
   );
