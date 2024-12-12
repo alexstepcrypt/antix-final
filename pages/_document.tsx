717cff06b87806a6489b69e1447e9094356e2986
alexstepcrypt <alexander.stepanchenko@crypterium.com>
2024-12-12 16:27:28 +0300

    new google tag manager script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -79,16 +79,18 @@ export default class MyDocument extends Document {
         />
       </Head>
       <body>
+        
         <Script strategy="afterInteractive"
           dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-M7DFJB2J');
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': 
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], 
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); 
+                })(window,document,'script','dataLayer','GTM-WRVZ3KR3');
               `,
           }}
         />
+
         <Main/>
         <NextScript/>
       </body>
