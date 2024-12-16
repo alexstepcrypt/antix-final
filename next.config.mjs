a5a961b233a2da77000c7cabc3fda4dc6f0879f4
alexstepcrypt <alexander.stepanchenko@crypterium.com>
2024-12-16 15:16:49 +0300

    Mobile deeplink

--- next.config.mjs
+++ next.config.mjs
@@ -18,6 +18,7 @@ const nextConfig = {
     images: {
         unoptimized: true, 
     },
+    trailingSlash: true,
     output: 'export',
     transpilePackages: ['gsap'],
     webpack: config => {
---
+++ pages/dashboard/wc.tsx
@@ -0,0 +1,6 @@
+import DashboardLayout from "./index";
+
+// Page for mobile wallets
+// when deeplink open on mobile, ios add to url /wc/ path
+
+export default DashboardLayout
