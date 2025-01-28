015fb86d9504990d7d4d2d856579dcfaed9d957f
alexstepcrypt <alexander.stepanchenko@crypterium.com>
2025-01-28 13:54:07 +0300

    fix

--- components/LunarNYModal/NYModal.tsx
+++ components/LunarNYModal/NYModal.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useRef } from 'react';
 import styles from './NY.module.scss';
-import Image from 'next/image';
+import { useConnectWallet } from '@/hooks/useConnectWallet';
 
 
 export default function NYModal() {
+  const { isConnected, connect } = useConnectWallet()
 
   const modalRef = useRef(null);
 
@@ -20,6 +21,12 @@ export default function NYModal() {
     sessionStorage.setItem('lunarNYModalClosed', 'true');
   }
 
+  function handleBuy() {
+    handleClose()
+    if (!isConnected) {
+      connect()
+    }
+  }
 
   return <dialog ref={modalRef} className={styles.nymodal}><section>
     <button className={styles.close} onClick={handleClose}></button>
@@ -30,7 +37,7 @@ export default function NYModal() {
     </p>
     <p>How many? That’s a secret. Buy now and find out!</p>
 
-    <button onClick={handleClose}>Buy now</button>
+    <button onClick={handleBuy}>Buy now</button>
   </section></dialog>
 };
 
